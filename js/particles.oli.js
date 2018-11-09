a0840d11e7f6e35e0c1fe4aa68d7e87069741f64
jwcnewton <jwcnewton@gmail.com>
2018-11-09 21:00:29 +0000

    Site updated: 2018-11-09 21:00:29

--- js/particles.oli.js
+++ js/particles.oli.js
@@ -1,99 +1,110 @@
 $(function () {
 
-    particlesJS("particles-oli-wrapper", {
-        "particles": {
-            "number": {
-                "value": 40,
-                "density": {
-                    "enable": true,
-                    "value_area": 800
-                }
-            },
-            "color": {
-                "value": "#399c9c"
-            },
-            "shape": {
-                "type": "circle",
-                "stroke": {
-                    "width": 0,
-                    "color": "#000000"
-                },
-                "polygon": {
-                    "nb_sides": 5
-                },
-                // "image": {
-                //     "src": "img/github.svg",
-                //     "width": 100,
-                //     "height": 100
-                // }
-            },
-            "opacity": {
-                "value": 0.3,
-                "random": false,
-                "anim": {
-                    "enable": false,
-                    "speed": 1,
-                    "opacity_min": 0.1,
-                    "sync": false
-                }
-            },
-            "size": {
-                "value": 20,
-                "random": true,
-                "anim": {
-                    "enable": false,
-                    "speed": 30,
-                    "size_min": 0.1,
-                    "sync": false
-                }
-            },
-            "line_linked": {
-                "enable": true,
-                "distance": 250,
-                "color": "#40afaf",
-                "opacity": 0.2,
-                "width": 1
-            },
-            "move": {
-                "enable": true,
-                "speed": 3,
-                "direction": "none",
-                "random": true,
-                "straight": false,
-                "out_mode": "out",
-                "bounce": true,
-                "attract": {
-                    "enable": false,
-                    "rotateX": 600,
-                    "rotateY": 1200
-                }
-            }
+    particlesJS("particles-oli-wrapper",{
+        "particles":{
+           "number":{
+              "value":6,
+              "density":{
+                 "enable":true,
+                 "value_area":800
+              }
+           },
+           "color":{
+              "value":"#1b1e34"
+           },
+           "shape":{
+              "type":"polygon",
+              "stroke":{
+                 "width":0,
+                 "color":"#000"
+              },
+              "polygon":{
+                 "nb_sides":6
+              }
+           },
+           "opacity":{
+              "value":0.3,
+              "random":true,
+              "anim":{
+                 "enable":false,
+                 "speed":1,
+                 "opacity_min":0.1,
+                 "sync":false
+              }
+           },
+           "size":{
+              "value":160,
+              "random":false,
+              "anim":{
+                 "enable":true,
+                 "speed":10,
+                 "size_min":40,
+                 "sync":false
+              }
+           },
+           "line_linked":{
+              "enable":false,
+              "distance":200,
+              "color":"#ffffff",
+              "opacity":1,
+              "width":2
+           },
+           "move":{
+              "enable":true,
+              "speed":8,
+              "direction":"none",
+              "random":false,
+              "straight":false,
+              "out_mode":"out",
+              "bounce":false,
+              "attract":{
+                 "enable":false,
+                 "rotateX":600,
+                 "rotateY":1200
+              }
+           }
         },
-        "interactivity": {
-            "detect_on": "canvas",
-            "events": {
-                "onhover": {
-                    "enable": true,
-                    "mode": "grab"
-                },
-                "onclick": {
-                    "enable": true,
-                    "mode": "push"
-                },
-                "resize": true
-            },
-            "modes": {
-                "grab": {
-                    "distance": 100,
-                    "line_linked": {
-                        "opacity": 0.5
-                    }
-                },
-
-            }
+        "interactivity":{
+           "detect_on":"canvas",
+           "events":{
+              "onhover":{
+                 "enable":false,
+                 "mode":"grab"
+              },
+              "onclick":{
+                 "enable":false,
+                 "mode":"push"
+              },
+              "resize":true
+           },
+           "modes":{
+              "grab":{
+                 "distance":400,
+                 "line_linked":{
+                    "opacity":1
+                 }
+              },
+              "bubble":{
+                 "distance":400,
+                 "size":40,
+                 "duration":2,
+                 "opacity":8,
+                 "speed":3
+              },
+              "repulse":{
+                 "distance":200,
+                 "duration":0.4
+              },
+              "push":{
+                 "particles_nb":4
+              },
+              "remove":{
+                 "particles_nb":2
+              }
+           }
         },
-        "retina_detect": true
-    });
+        "retina_detect":true
+     });
 
     var $particle = $('#particles-oli-wrapper').css({ display: 'none' }).removeClass('hide');
     var $navH1 = $('.index-wrapper header h1, .index-wrapper header nav').hide();
